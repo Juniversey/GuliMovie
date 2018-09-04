b107a75862519611beeedb40dfe64b638d88cd73
赵佳星 <zjx-63@163.com>
2018-09-04 11:20:23 +0800

    create admin route

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://root:888888@localhost/movie"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+from .admin import admin as admin_blueprint
+from .home import home as home_blueprint
+
+app.register_blueprint(home_blueprint)
+app.register_blueprint(admin_blueprint, url_prefix='/admin')
